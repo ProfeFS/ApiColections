d451dff0c4f456a021aa4063f811284edee094d7
ProfeFS <profefs@outlook.com>
2024-03-21 15:02:31 +0100

    Ejemplos de orden

--- src/main/java/es/edu/cesur/programacion/mapas/EjemploHasMap.java
+++ src/main/java/es/edu/cesur/programacion/mapas/EjemploHasMap.java
@@ -3,34 +3,39 @@ package es.edu.cesur.programacion.mapas;
 import java.nio.file.spi.FileSystemProvider;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class EjemploHasMap {
 
 	public static void main(String[] args) {
 		Map<String, Integer> edadeAlumnos = new HashMap<>();
-		
+
 		edadeAlumnos.put("Andrés", 35);
 		edadeAlumnos.put("Iván", 35);
 		edadeAlumnos.put("Carmen", 30);
 		edadeAlumnos.put("Andrés", 30);
-		
+
 		int edad = edadeAlumnos.get("Andrés");
-		System.out.println("La edad de andrés es: " +edad);
-		
-		
-		for(Map.Entry<String, Integer> entrada : edadeAlumnos.entrySet() ) {
-			//System.out.println(entrada.getKey() + ": " + entrada.getValue());
-			List<String> clavesEncontradas = new ArrayList<>();
-			if(entrada.getValue() == edad) {
-				clavesEncontradas.add(entrada.getKey());
-				System.out.println(entrada.getKey() + ": " + entrada.getValue());
+		System.out.println("La edad de andrés es: " + edad);
+
+		Set<String> clavesEncontradas = new HashSet<>();
+		for (Entry<String, Integer> par : edadeAlumnos.entrySet()) {
+			// System.out.println(entrada.getKey() + ": " + entrada.getValue());
+			if (par.getValue() == edad) {
+				clavesEncontradas.add(par.getKey());
 			}
 		}
-		
-		if(edadeAlumnos.containsKey("Andrés")) {
+
+		for (String clave : clavesEncontradas) {
+			System.out.println(clave);
+		}
+
+		if (edadeAlumnos.containsKey("Andrés")) {
 			System.out.println(edadeAlumnos.get("Andrés"));
 		}
 
