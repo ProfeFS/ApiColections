4b1ea54af18ec9ee2a6ae49bcd0390e03685cfca
ProfeFS <profefs@outlook.com>
2024-04-05 12:51:45 +0200

    formateado

--- src/main/java/es/edu/cesur/programacion/utils/TextUtil.java
+++ src/main/java/es/edu/cesur/programacion/utils/TextUtil.java
@@ -4,10 +4,11 @@ import java.text.Normalizer;
 
 public class TextUtil {
     public static String normalizarTexto(String texto) {
-        if (texto == null) {
-            return null;
-        }
-        //return texto.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "").toLowerCase();
-        return Normalizer.normalize(texto, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").toLowerCase();
+	if (texto == null) {
+	    return null;
+	}
+	// return texto.replaceAll("[\\p{InCombiningDiacriticalMarks}]",
+	// "").toLowerCase();
+	return Normalizer.normalize(texto, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").toLowerCase();
     }
 }
