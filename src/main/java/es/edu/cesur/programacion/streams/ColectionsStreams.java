8eaedcf5ffe22e1319de66b26f9f6cfec8388190
ProfeFS <profefs@outlook.com>
2024-04-11 13:55:24 +0200

    cleaning

--- src/main/java/es/edu/cesur/programacion/streams/ColectionsStreams.java
+++ src/main/java/es/edu/cesur/programacion/streams/ColectionsStreams.java
@@ -2,11 +2,7 @@ package es.edu.cesur.programacion.streams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import es.edu.cesur.programacion.comparators.NombrePersonaComparatos;
